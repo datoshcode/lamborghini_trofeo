be5ed63789d5c1218e7a9443232eef04adb00218
Datosh <datoshcode@gmail.com>
2022-12-01 13:14:05 +0200

    added footer

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Swiper slider
 const swiper = new Swiper(".swiper", {
   // Optional parameters
   loop: true,
@@ -10,3 +11,140 @@ const swiper = new Swiper(".swiper", {
     prevEl: ".swiper-button-left",
   },
 });
+
+// Map
+let map;
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.231555640037385, lng: -8.628274312011456 },
+    zoom: 13.6,
+    styles: [
+      {
+        featureType: "administrative",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "administrative.province",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "off",
+          },
+        ],
+      },
+      {
+        featureType: "landscape",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100,
+          },
+          {
+            lightness: 65,
+          },
+          {
+            visibility: "on",
+          },
+        ],
+      },
+      {
+        featureType: "poi",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100,
+          },
+          {
+            lightness: "50",
+          },
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "road",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: "-100",
+          },
+        ],
+      },
+      {
+        featureType: "road.highway",
+        elementType: "all",
+        stylers: [
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "road.arterial",
+        elementType: "all",
+        stylers: [
+          {
+            lightness: "30",
+          },
+        ],
+      },
+      {
+        featureType: "road.local",
+        elementType: "all",
+        stylers: [
+          {
+            lightness: "40",
+          },
+        ],
+      },
+      {
+        featureType: "transit",
+        elementType: "all",
+        stylers: [
+          {
+            saturation: -100,
+          },
+          {
+            visibility: "simplified",
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "geometry",
+        stylers: [
+          {
+            hue: "#ffff00",
+          },
+          {
+            lightness: -25,
+          },
+          {
+            saturation: -97,
+          },
+        ],
+      },
+      {
+        featureType: "water",
+        elementType: "labels",
+        stylers: [
+          {
+            lightness: -25,
+          },
+          {
+            saturation: -100,
+          },
+        ],
+      },
+    ],
+  });
+}
+
+window.initMap = initMap;
